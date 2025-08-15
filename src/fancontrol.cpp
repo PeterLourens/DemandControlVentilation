7a3492c17c2c8ef4e0979faba322c3f504470f5a
peter <peter@lourens.cloud>
2025-08-15 10:33:47 +0200

    changed fancontrol for global path

--- src/fancontrol.cpp
+++ src/fancontrol.cpp
@@ -1,110 +1,119 @@
 #include "fancontrol.h"
 
-void set_fanspeed(String speed) {
-    
+void set_fanspeed(String speed)
+{
+
     int httpResponseCode = 0;
     bool settings_file_present = 0;
-    const char* path = "/json/settings_fan.json";
-    
-    String fan_control_settings_str = "";
+
+    String settings_fan_string = "";
     String fanspeed_temp = "";
     String message = "";
     String http_payload = "";
 
     HTTPClient http;
-    JsonDocument doc;  
+    JsonDocument doc;
 
-    if (fanspeed_mutex != NULL) {
-        if(xSemaphoreTake(fanspeed_mutex, ( TickType_t ) 100 ) == pdTRUE) {
+    if (fanspeed_mutex != NULL)
+    {
+        if (xSemaphoreTake(fanspeed_mutex, (TickType_t)100) == pdTRUE)
+        {
             fanspeed_temp = fanspeed;
             xSemaphoreGive(fanspeed_mutex);
         }
     }
 
-    if (settings_fan_mutex != NULL) {
-        if(xSemaphoreTake(settings_fan_mutex, ( TickType_t ) 100 ) == pdTRUE) {
-            settings_file_present = check_file_exists(path);
-            if (settings_file_present == 1) {
-                fan_control_settings_str = read_config_file(path);
-                deserializeJson(doc, fan_control_settings_str);
+    if (settings_fan_mutex != NULL)
+    {
+        if (xSemaphoreTake(settings_fan_mutex, (TickType_t)100) == pdTRUE)
+        {
+            settings_file_present = check_file_exists(SETTINGS_FAN_PATH);
+            if (settings_file_present == 1)
+            {
+                settings_fan_string = read_config_file(SETTINGS_FAN_PATH);
             }
             xSemaphoreGive(settings_fan_mutex);
         }
     }
-    
+
+    if (settings_fan_string == "")
+    {
+        message = "[ERROR] String is empty or failed to read file";
+        print_message(message);
+        return;
+    }
+    else
+    {
+        DeserializationError err = deserializeJson(doc, settings_fan_string);
+        if (err)
+        {
+            message = "[ERROR] Failed to parse: " + String(SETTINGS_FAN_PATH) + " with error: " + String(err.c_str());
+            print_message(message);
+            return "";
+        }
+    }
+
     String fan_control_mode = doc[String("fan_control_mode")];
     message = "Fanspeed: " + String(fanspeed_temp);
     print_message(message);
-        
-    if (fan_control_mode == "MQTT publish") {
+
+    if (fan_control_mode == "MQTT publish")
+    {
         message = "Using MQTT to set fan speed";
         print_message(message);
         String fan_control_mqtt_topic = doc[String("fan_control_mqtt_topic")];
-        //Not yet implmented, subscribe to MQTT topic, Create callback function
+        // Not yet implmented, subscribe to MQTT topic, create callback function
     }
-    else if (fan_control_mode == "HTTP API") {
+    else if (fan_control_mode == "HTTP API")
+    {
         message = "Using HTTP API to set fan speed";
         print_message(message);
         String fan_control_url_high_speed = doc[("fan_control_url_high_speed")];
         String fan_control_url_medium_speed = doc[("fan_control_url_medium_speed")];
         String fan_control_url_low_speed = doc[("fan_control_url_low_speed")];
-        if (fanspeed_temp == "Low") {
+        if (fanspeed_temp == "Low")
+        {
             message = "Low speed selected";
             print_message(message);
             http.begin(fan_control_url_low_speed.c_str());
             httpResponseCode = http.GET();
-            if (httpResponseCode>0) {
+            if (httpResponseCode > 0)
+            {
                 http_payload = http.getString();
                 message = "HTTP Response code: " + String(httpResponseCode) + ", HTTP payload: " + String(http_payload);
                 print_message(message);
             }
-
         }
-        else if (fanspeed_temp == "Medium") {
+        else if (fanspeed_temp == "Medium")
+        {
             message = "Medium speed selected";
             print_message(message);
             http.begin(fan_control_url_medium_speed.c_str());
             httpResponseCode = http.GET();
-            if (httpResponseCode>0) {
+            if (httpResponseCode > 0)
+            {
                 http_payload = http.getString();
                 message = "HTTP Response code: " + String(httpResponseCode) + ", HTTP payload: " + String(http_payload);
                 print_message(message);
             }
         }
-        else if (fanspeed_temp == "High") {
+        else if (fanspeed_temp == "High")
+        {
             message = "High speed selected";
             print_message(message);
             http.begin(fan_control_url_high_speed.c_str());
             httpResponseCode = http.GET();
-            if (httpResponseCode>0) {
+            if (httpResponseCode > 0)
+            {
                 http_payload = http.getString();
                 message = "HTTP Response code: " + String(httpResponseCode) + ", HTTP payload: " + String(http_payload);
                 print_message(message);
             }
         }
     }
-    else {
+    else
+    {
         message = "No method to set fan speed.";
         print_message(message);
     }
-
-
-
-    /*
-    String server_name = "http://192.168.40.74/api.html?vremotecmd=";
-    String server_path = server_name + speed;
-
-    http.begin(server_path.c_str());
-
-    //int httpResponseCode = 0;
-    int httpResponseCode = http.GET();
-      
-    if (httpResponseCode>0) {
-        Serial.print("HTTP Response code: ");
-        Serial.println(httpResponseCode);
-        String payload = http.getString();
-        Serial.print("\t\t" + payload);
-    }
-
-    */
 }
