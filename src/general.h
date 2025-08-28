8c940b16bc98a31e3c9a7d6b2e19d986415d99f4
Peter Lourens <peter@lourens.cloud>
2025-08-28 12:07:51 +0200

    add functions for weekend and day or night selection

--- src/general.h
+++ src/general.h
@@ -16,5 +16,7 @@ String formatted_uptime(void);
 void sensor_data_average(void);
 void fifoPush(int, int, int, float, int);
 float roundToTwoDecimals(float);
+bool is_weekend(void);
+bool is_day_or_night(void);
 
 #endif
